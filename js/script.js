a351b2827ac55dc0b8645b2f8198006357660134
11mishanik11 <93082995+11mishanik11@users.noreply.github.com>
2023-01-13 16:07:38 +0300

    posh-favicon_slider-slick-intro
    posh-favicon_slider-slick-intro

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 const burgerBtn = document.querySelector('#burger-btn');
 const header = document.querySelector('#header');
-
 const scrollBtn = document.querySelectorAll('#scroll-btn');
 
 burgerBtn.onclick = burgerShow;
+
 function burgerShow() {
     header.classList.toggle('active');
     document.body.classList.toggle('hidden');
 }
 
-
 for (let i = 0; i < scrollBtn.length; i++) {
     scrollBtn[i].onclick = scrollToBlock;
 }
 
-function scrollToBlock() {
+function scrollToBlock(event) {
     event.preventDefault();
     let blockId = this.dataset.element;
     let blockElement = document.querySelector(blockId);
@@ -24,3 +23,30 @@ function scrollToBlock() {
         behavior: 'smooth'
       });
 }
+
+
+
+
+
+
+
+// JQuery ---------------------------------------------------
+$( window ).on( "load", function() { 
+
+// Slick slider
+    $('.slider-intro').slick({
+        fade: true,
+        arrows: false,
+
+        autoplay: true,
+        autoplaySpeed: 5000,
+
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        touchThreshold: 15,
+        verticalSwiping: true,
+
+        dots: true,
+        appendDots: (".intro__wrapper")
+      });
+ });
