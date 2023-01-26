d223bb1399c3de689dbe5172be0ed54390d74458
Mishail <93082995+11mishanik11@users.noreply.github.com>
2023-01-26 12:03:56 +0300

    Валидация

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 let burgerBtn = document.querySelector('#burger-btn');
 let header = document.querySelector('#header');
-let scrollBtn = document.querySelectorAll('#scroll-btn');
+let scrollBtn = document.querySelectorAll('[data-btn-scroll]');
 
 // -------------------
 window.onscroll = headerFixed;
 
 // -------------------
 burgerBtn.onclick = () => {
-    addClassHeader('active', true);
+    header.classList.toggle('active');
+    document.querySelector('body').classList.toggle('hidden')
 };
 
 // -------------------
@@ -15,25 +16,16 @@ for (let i = 0; i < scrollBtn.length; i++) {
     scrollBtn[i].onclick = scrollToBlock;
 }
 
-
-// Добавление класса в шапку
-function addClassHeader(className, hiddenBody = false) {
-    header.classList.toggle(className);
-
-    // Если второй аргумент "true"
-    if(hiddenBody) {
-        document.body.classList.toggle('hidden');
-    }
-}
-
 // Скролл до блока по нажатию
 function scrollToBlock(event) {
     event.preventDefault();
-    let blockId = this.dataset.element;
-    let blockElement = document.querySelector(blockId);
-    let blockScrollValue = blockElement.offsetTop - header.offsetHeight;
+    let blockId = this.dataset.btnScroll;
+    let block = document.querySelector(blockId)
+    let blockScrollValue = block.offsetTop - header.offsetHeight;
+
     header.classList.remove('active')
     document.body.classList.remove('hidden')
+    
     window.scrollTo({
         top: blockScrollValue,
         behavior: 'smooth'
